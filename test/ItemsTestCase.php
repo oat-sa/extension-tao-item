4b5555ea663e775e76eb7c71fb47d62602e191de
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-03-18 14:09:38 +0000

    * Update layout: container size and positions  * Update Unit Tests
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoItems/trunk@2457 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/ItemsTestCase.php
+++ test/ItemsTestCase.php
@@ -37,6 +37,52 @@ class ItemsTestCase extends UnitTestCase {
 		$this->itemsService = $itemsService;
 	}
 	
-
+	/**
+	 * Usual CRUD (Create Read Update Delete) on the item class  
+	 */
+	public function testCrud(){
+		
+		//check parent class
+		$this->assertTrue(defined('TAO_ITEM_CLASS'));
+		$itemClass = $this->itemsService->getItemClass();
+		$this->assertIsA($itemClass, 'core_kernel_classes_Class');
+		$this->assertEqual(TAO_ITEM_CLASS, $itemClass->uriResource);
+		
+		//create a subclass
+		$subItemClassLabel = 'subItem class';
+		$subItemClass = $this->itemsService->createSubClass($itemClass, $subItemClassLabel);
+		$this->assertIsA($subItemClass, 'core_kernel_classes_Class');
+		$this->assertEqual($subItemClassLabel, $subItemClass->getLabel());
+		$this->assertTrue($this->itemsService->isItemClass($subItemClass));
+		
+		//create an instance of the Item class
+		$itemInstanceLabel = 'item instance';
+		$itemInstance = $this->itemsService->createInstance($itemClass, $itemInstanceLabel);
+		$this->assertIsA($itemInstance, 'core_kernel_classes_Resource');
+		$this->assertEqual($itemInstanceLabel, $itemInstance->getLabel());
+		
+		//create instance of subItem
+		$subItemInstanceLabel = 'subItem instance';
+		$subItemInstance = $this->itemsService->createInstance($subItemClass);
+		$this->assertTrue(defined('RDFS_LABEL'));
+		$subItemInstance->setPropertyValue(new core_kernel_classes_Property(RDFS_LABEL), $subItemInstanceLabel);
+		$this->assertIsA($subItemInstance, 'core_kernel_classes_Resource');
+//		$this->assertEqual($subItemInstanceLabel, $subItemInstance->getLabel());
+		
+		$subItemInstanceLabel2 = 'my sub item instance';
+		$subItemInstance->setLabel($subItemInstanceLabel2);
+		$this->assertEqual($subItemInstanceLabel2, $subItemInstance->getLabel());
+		
+		//delete group instance
+		$this->assertTrue($itemInstance->delete());
+		
+		//delete subclass and check if the instance is deleted
+		$subItemInstanceUri = $subItemInstance->uriResource;
+		$this->assertNotNull($this->itemsService->getItem($subItemInstanceUri));
+		$this->assertTrue($subItemInstance->delete());
+		$this->assertNull($this->itemsService->getItem($subItemInstanceUri));
+		
+		$this->assertTrue($subItemClass->delete());
+	}
 }
 ?>
