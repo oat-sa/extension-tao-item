efc2bc82faaaa9a59b2179e97ddafcec76317aef
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-06-16 15:49:55 +0000

    * update htaccess: remove indexes option  * update taoQual reviewing services  * update wfEngine UI
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoItems/trunk@2776 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/ext/itemAuthoring/index.php
+++ models/ext/itemAuthoring/index.php
@@ -4,9 +4,9 @@
  * @todo REMOVE THIS AND REDIRECT DIRECTLY TO A VALID ACTION
  */
 
-require_once('../../../../generis/common/inc.extension.php');
-require_once('../../../includes/common.php');
-require_once('../../../../tao/helpers/class.Uri.php');
+require_once(dirname(__FILE__).'/../../../../generis/common/inc.extension.php');
+require_once(dirname(__FILE__).'/../../../includes/common.php');
+require_once(ROOT_PATH.'/tao/helpers/class.Uri.php');
 
 /* BRIDGE WITH THE LEGACY FORMS */
 if(isset($_POST['itemcontent'])){
@@ -20,9 +20,9 @@ else{
 $_SESSION['instance'] = $_POST['instance'];
 
 if(preg_match("/SaSItems/", $_SERVER['REQUEST_URI'])){
-	header("Location: "._url('saveItemContent', 'SaSItems', 'taoItems'));
+	header("Location: ".tao_helpers_Uri::url('saveItemContent', 'SaSItems', 'taoItems'));
 }
 else{
-	header("Location: "._url('saveItemContent', 'Items', 'taoItems'));
+	header("Location: ".tao_helpers_Uri::url('saveItemContent', 'Items', 'taoItems'));
 }
 ?>
