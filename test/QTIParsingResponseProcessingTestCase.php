1cc3fb6611cdaff7fa189461dd07813753663718
calfonsi <calfonsi@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-10-21 09:31:38 +0000

    UPDATE Test case
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoItems/trunk@3497 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/QTIParsingResponseProcessingTestCase.php
+++ test/QTIParsingResponseProcessingTestCase.php
@@ -20,57 +20,25 @@ class QTIParsingResponseProcessingTestCase extends UnitTestCase {
 		$this->qtiService = tao_models_classes_ServiceFactory::get("taoItems_models_classes_QTI_Service");
 	}
 	
-	
 	/**
 	 * test qti file parsing: validation and loading in a non-persistant context
 	 */
-	public function testFileParsing(){
-		return;
+	public function testMatching (){
 		taoItems_models_classes_QTI_Data::setPersistance(false);
 		
 		//check if samples are loaded
-		$file = dirname(__FILE__).'/samples/choice.xml'; 
+		$file = dirname(__FILE__).'/samples/associate.xml'; 
 
 		$qtiParser = new taoItems_models_classes_QTI_Parser($file);
 		$qtiParser->validate();
 		
 		$this->assertTrue($qtiParser->isValid());
 		$item = $qtiParser->load();
-   //     echo '<pre>'; print_r( $item->getResponseProcessing ()); echo '</pre>';
-//        echo '<pre>'; print_r ($item); echo '</pre>';
 		
-//		$qtiRes = $item->toQTI ();
+		echo $item->toXHTML();
 		
 		$this->assertIsA($item, 'taoItems_models_classes_QTI_Item');
 	}
-	
-	/**
-	 * test the building an QTI_Item object from it's XML definition
-	 */
-//	public function testBuilding(){
-//		
-//		taoItems_models_classes_QTI_Data::setPersistance(false);
-//		
-//		$file = dirname(__FILE__).'/samples/choice.xml'; 
-//		$qtiParser = new taoItems_models_classes_QTI_Parser($file);
-//		$item = $qtiParser->load();
-//		
-//		$this->assertTrue($qtiParser->isValid());
-//		$this->assertNotNull($item);
-//		$this->assertIsA($item, 'taoItems_models_classes_QTI_Item');
-//		
-////		echo '<pre>';
-////		print_r ($item);
-////		echo '</pre>';
-//		
-//		foreach($item->getInteractions() as $interaction){
-//			$this->assertIsA($interaction, 'taoItems_models_classes_QTI_Interaction');
-//			
-//			foreach($interaction->getChoices() as $choice){
-//				$this->assertIsA($choice, 'taoItems_models_classes_QTI_Choice');
-//			}
-//		}
-//	}
 	
 }
 ?>
