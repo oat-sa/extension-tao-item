570d640f66ecde6418f3e5bb70f990b1044ac3d0
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-02-18 16:30:37 +0000

    Update migration item scripts
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoItems/trunk@4673 62dfeb86-a93f-4914-9d13-15c0a160985d

--- scripts/class.MigrateLegacyItems.php
+++ scripts/class.MigrateLegacyItems.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
  *
  * This file is part of TAO.
  *
- * Automatically generated on 17.02.2011, 15:00:00 with ArgoUML PHP module 
+ * Automatically generated on 18.02.2011, 16:45:07 with ArgoUML PHP module 
  * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
  *
  * @author Bertrand Chevrier, <bertrand.chevrier@tudor.lu>
@@ -52,7 +52,43 @@ class taoItems_scripts_MigrateLegacyItems
 
     // --- ATTRIBUTES ---
 
+    /**
+     * Short description of attribute item
+     *
+     * @access private
+     * @var Resource
+     */
+    private $item = null;
+
     // --- OPERATIONS ---
+
+    /**
+     * Short description of method preRun
+     *
+     * @access protected
+     * @author Bertrand Chevrier, <bertrand.chevrier@tudor.lu>
+     * @return mixed
+     */
+    protected function preRun()
+    {
+        // section 127-0-1-1--5d5119d4:12e3924f2ec:-8000:0000000000002D81 begin
+        
+    if(isset($this->inputFormat['uri'])){
+    		$this->item = $this->getResource($this->parameters['uri']);
+    	}
+    	if(is_null($this->item) && isset($this->parameters['addResource']) && $this->parameters['addResource'] === true){
+    		$clazz = null;
+    		if(isset($this->parameters['class'])){
+    			$clazz = new core_kernel_classes_Class($this->parameters['class']);
+    		}
+    		$this->item = $this->createResource($clazz);
+    	}
+    	if ((isset($this->inputFormat['uri'])  || isset($this->parameters['addResource'])) && is_null($this->item )){
+    		self::err("Unable to create/retrieve item");
+    	}
+    	
+        // section 127-0-1-1--5d5119d4:12e3924f2ec:-8000:0000000000002D81 end
+    }
 
     /**
      * Short description of method run
@@ -65,13 +101,116 @@ class taoItems_scripts_MigrateLegacyItems
     {
         // section 127-0-1-1--39e3a8dd:12e33ba6c22:-8000:0000000000002D6E begin
         
-    	try{
-	    	$dom = new DOMDocument();
-	    	$dom->load($this->parameters['input']);
+    	$this->qcm2Qti($this->parameters['input']);
     	
-	    	$inqueries = array();
-	    	
+    	
+        // section 127-0-1-1--39e3a8dd:12e33ba6c22:-8000:0000000000002D6E end
+    }
+
+    /**
+     * Short description of method createResource
+     *
+     * @access private
+     * @author Bertrand Chevrier, <bertrand.chevrier@tudor.lu>
+     * @param  Class clazz
+     * @return core_kernel_classes_Resource
+     */
+    private function createResource( core_kernel_classes_Class $clazz = null)
+    {
+        $returnValue = null;
+
+        // section 127-0-1-1--5d5119d4:12e3924f2ec:-8000:0000000000002D76 begin
+        
+        if(is_null($clazz)){
+        	$clazz = new core_kernel_classes_Class(TAO_ITEM_CLASS);
+        }
+        $itemService = tao_models_classes_ServiceFactory::get("items");
+        if($itemService->isItemClass($clazz)){
+        	$returnValue = $clazz->createInstance();
+        }
+        // section 127-0-1-1--5d5119d4:12e3924f2ec:-8000:0000000000002D76 end
+
+        return $returnValue;
+    }
+
+    /**
+     * Short description of method getResource
+     *
+     * @access private
+     * @author Bertrand Chevrier, <bertrand.chevrier@tudor.lu>
+     * @param  string uri
+     * @return core_kernel_classes_Resource
+     */
+    private function getResource($uri)
+    {
+        $returnValue = null;
+
+        // section 127-0-1-1--5d5119d4:12e3924f2ec:-8000:0000000000002D7E begin
+        
+        $itemService = tao_models_classes_ServiceFactory::get("items");
+     
+        $returnValue = new core_kernel_classes_Resource($uri);
+        
+        $isItem = false;
+        $types = $returnValue->getPropertyValuesCollection(new core_kernel_classes_Property(RDF_TYPE));
+        foreach($types->getIterator() as $type){
+        	if($itemService->isItemClass($type)){
+        			 $isItem = true;
+        			 break;
+        	}
+        }
+        if(!$isItem){
+        	$returnValue = null;
+        }
+        
+        // section 127-0-1-1--5d5119d4:12e3924f2ec:-8000:0000000000002D7E end
+
+        return $returnValue;
+    }
+
+    /**
+     * Short description of method qcm2Qti
+     *
+     * @access private
+     * @author Bertrand Chevrier, <bertrand.chevrier@tudor.lu>
+     * @param  string inputFile
+     * @return string
+     */
+    private function qcm2Qti($inputFile)
+    {
+        $returnValue = (string) '';
+
+        // section 127-0-1-1--5d5119d4:12e3924f2ec:-8000:0000000000002D78 begin
+        
+	    try{
+		    $dom = new DOMDocument();
+		    $dom->load($inputFile);
 	    	$xpath = new DOMXPath($dom);
+		    	
+		    //create default structure
+		    $itemIdentifier = null;
+		    if(!is_null($this->item)){
+		    	$itemIdentifier = tao_helpers_Uri::getUniqueId($this->item->uriResource);
+		    }
+		    
+		    $label = '';
+		    foreach($xpath->query("rdfs:LABEL") as $labelNode){
+		    	$label = $labelNode->nodeValue;
+		    	break;
+		    }
+		    
+		    $qtiItem = new taoItems_models_classes_QTI_Item($itemIdentifier, array('title' => $label));
+			
+			//add default responseProcessing:
+			$qtiItem->setOutcomes(array(
+				new taoItems_models_classes_QTI_Outcome('SCORE', array('baseType' => 'integer', 'cardinality' => 'single'))
+			));
+			$qtiItem->setResponseProcessing(
+				new taoItems_models_classes_QTI_response_TemplatesDriven()
+			);
+		
+	    	
+	    	$inqueries = array();
 	    	foreach($xpath->query("//tao:INQUIRY") as $inquery){
 	    		$added = false;
 	    		if($inquery->getAttribute('order')){
@@ -85,13 +224,76 @@ class taoItems_scripts_MigrateLegacyItems
 	    			$inqueries[max(array_keys($inqueries)) + 1] = $inquery;
 	    		}
 	    	}
-	    	print_r($inqueries);
+	    	ksort($inqueries);
+	    	
+	    	$interactions = array();
+	    	foreach($inqueries as $inquery){
+	    		
+				$interaction  = new taoItems_models_classes_QTI_Interaction('choice');
+	    		foreach($xpath->query("tao:QUESTION", $inquery) as $question){
+	    			$interaction->setPrompt($question->nodeValue);
+	    		}
+	    		foreach($xpath->query("tao:PROPOSITIONTYPE") as $propositionType){
+	    			if(preg_match("/^Exclusive/i", trim($propositionType->nodeValue))){
+	    				$interaction->setOption("maxChoice", 1);
+	    			}
+	    			if(preg_match("/^Multiple/i", trim($propositionType->nodeValue))){
+	    				$interaction->setOption("maxChoice", 0);
+	    			}
+	    		}
+	    		
+	    		
+	    		$choices = array();
+	    		foreach($xpath->query("//tao:PROPOSITION", $inquery) as $proposition){
+	    			$identifier = null;
+	    			if($proposition->hasAttribute('Id')){
+	    				$identifier = $proposition->getAttribute('Id');
+	    			}
+	    			
+	    			$choice = new taoItems_models_classes_QTI_Choice($identifier);
+		    		$choice->setType('simpleChoice');
+		    		
+		    		$choice->setData($proposition->nodeValue);
+		    		
+	    			$added = false;
+		    		if($proposition->getAttribute('order')){
+		    			$order = (int)$proposition->getAttribute('order');
+		    			if(!array_key_exists($order, $choices)){
+		    				$choices[(int)$proposition->getAttribute('order')] = $choice;
+		    				$added = true;
+		    			}
+		    		}
+		    		if(!$added){
+		    			$choices[max(array_keys($inqueries)) + 1] = $choice;
+		    		}
+	    		}
+	    		ksort($choices);
+	    		$interaction->setChoices($choices);
+	    		
+	    		$data = '';
+	    		foreach($choices as $choice){
+	    			$data .= '{'.$choice->getSerial().'}';	
+	    		}
+	    		$interaction->setData($data);
+	    		$interactions[] = $interaction;
+	    	}
+	    	
+	    	$qtiItem->setInteractions($interactions);
+	   	 	$data = '';
+	    	foreach($interactions as $interaction){
+	    		$data .= '{'.$interaction->getSerial().'}';	
+	    	}
+	    	$qtiItem->setData($data);
+	    		
+	    	print_r($qtiItem);
     	}
     	catch(DomainException $de){
 			self::err($de, true);    		
     	}
-    	
-        // section 127-0-1-1--39e3a8dd:12e33ba6c22:-8000:0000000000002D6E end
+        
+        // section 127-0-1-1--5d5119d4:12e3924f2ec:-8000:0000000000002D78 end
+
+        return (string) $returnValue;
     }
 
 } /* end of class taoItems_scripts_MigrateLegacyItems */
--- scripts/migrateLegacy.php
+++ scripts/migrateLegacy.php
@@ -3,14 +3,43 @@ require_once dirname(__FILE__) . '/../includes/raw_start.php';
 
 new taoItems_scripts_MigrateLegacyItems(array(
 	'min'		=> 2,
-	'required'	=> array('input', 'output'),
-	'types'		=> array(
-		'input'		=> 'file',
-		'output' 	=> 'path'
+	'required'	=> array(
+		array('input', 'output'),
+		array('input', 'uri'),
+		array('input', 'addResource')
 	),
-	'shortcuts'	=> array(
-		'input'		=> 'i',
-		'output' 	=> 'o'
+	'parameters' => array(
+		array(
+			'name' 			=> 'input',
+			'type' 			=> 'file',
+			'shortcut'		=> 'i',
+			'required'		=> true,
+			'description'	=> 'the intput file containing the legacy item'
+		),
+		array(
+			'name' 			=> 'output',
+			'type' 			=> 'path',
+			'shortcut'		=> 'o',
+			'description'	=> 'the output file to save the new item'
+		),
+		array(
+			'name' 			=> 'uri',
+			'type' 			=> 'string',
+			'shortcut'		=> 'u',
+			'description' 	=> 'the uri of an existing resource to bind the item content'
+		),
+		array(
+			'name' 			=> 'addResource',
+			'type' 			=> 'boolean',
+			'shortcut'		=> 'a',
+			'description' 	=> 'create a new resource to bind the item content'
+		),
+		array(
+			'name' 			=> 'class',
+			'type' 			=> 'string',
+			'shortcut'		=> 'c',
+			'description' 	=> 'the RDFS class where to add the resource'
+		)
 	)
 ));
 ?>
