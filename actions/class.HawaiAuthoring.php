9b60cb7398247e9b2e6143f3e15f832c1b9a93dc
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-03-23 08:48:00 +0000

    add HAWAI saving action
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoItems/trunk@5058 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/class.HawaiAuthoring.php
+++ actions/class.HawaiAuthoring.php
@@ -23,9 +23,21 @@ class taoItems_actions_HawaiAuthoring extends tao_actions_CommonModule {
 	 * Save hawai item content
 	 */
 	public function saveItemContent() {
-		$_SESSION['xml'] = $_POST['xml'];
-		$_SESSION['instance'] = $_POST['instance'];
-		header("Location: ".tao_helpers_Uri::url('saveItemContent', 'Items', 'taoItems'));
+		if($this->hasRequestParameter("xml") && $this->hasRequestParameter("isntance")){
+			$item = $this->itemService->getItem($this->getRequestParameter("isntance"));
+			if(!is_null($item) && $this->itemService->isItemModelDefined($item)){
+				$item = $this->itemService->setItemContent($item, $data);
+				$this->setSessionAttribute("showNodeUri", tao_helpers_Uri::encode($item->uriResource));
+				$message = __('Item saved successfully');
+				if(tao_helpers_Context::check('STANDALONE_MODE')){
+					$itemClass = $this->service->getClass($item);
+					$this->redirect(_url('authoring', 'SaSItems', 'taoItems', array('uri' => tao_helpers_Uri::encode($item->uriResource).'&classUri='.tao_helpers_Uri::encode($itemClass->uriResource), 'classUri' => tao_helpers_Uri::encode($itemClass->uriResource), 'message' => urlencode($message))));
+				}
+				else{
+					$this->redirect( _url('index', 'Main', 'tao', array('message' => urlencode($message))));
+				}
+			}
+		}
 	}
 }
 
