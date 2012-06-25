34d883659b3456f873516d655583d936b384d948
jbihin <jbihin@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-06-25 08:37:33 +0000

    fixed #1330
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoItems/trunk@11603 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiAuthoring/tinyCarousel.js
+++ views/js/qtiAuthoring/tinyCarousel.js
@@ -2,7 +2,7 @@ tinyCarousel.instances = [];
 function tinyCarousel(container, content, next, prev, options){
 
 	// var defaultOptions = {
-		// total:<?=$total?>,
+		// total:<?=$total?>, //? not used, right ?
 		// current:0,
 		// containerWidth:0,
 		// contentWidth:0,
@@ -10,113 +10,114 @@ function tinyCarousel(container, content, next, prev, options){
 		// container: '#container',
 		// content:'#content'
 	// };
-	
+
 	this.current = 0;
 	this.options = options;
 	// this.total = parseInt(total);
 	this.container = container;
 	this.content = content;
-	
+
 	//count the number of elements in container:
-	var total = null;
-	if(!total){
-		this.total = $(this.content).children('div').length;
-		if(this.total){
-			var $refElt = $($(this.content).children('div')[0]);
-			this.stepWidth = parseInt($refElt.width()); 
-			// this.stepWidth += parseInt($refElt.css('border-left')); 
-			// this.stepWidth += parseInt($refElt.css('border-right')); 
-			this.stepWidth += parseInt($refElt.css('padding-left'));
-			this.stepWidth += parseInt($refElt.css('padding-right'));
+	/*var total = null;
+	if(!total){*/
+		$divs = $(this.content+' div');
+		this.total = $divs.length;
+		//if(this.total){
+			var $refElt = $($divs[0]);
+			this.stepWidth = parseInt($refElt.innerWidth());
+			// this.stepWidth += parseInt($refElt.css('border-left'));
+			// this.stepWidth += parseInt($refElt.css('border-right'));
+			//this.stepWidth += parseInt($refElt.css('padding-left'));
+			//this.stepWidth += parseInt($refElt.css('padding-right'));
 			// this.stepWidth += parseInt($refElt.css('margin-left'));
 			// this.stepWidth += parseInt($refElt.css('margin-right'));
-						
+
 			this.contentWidth = this.total * this.stepWidth;
-			
+
 			$(this.content).width(this.contentWidth);
 			$(this.container).css('overflow', 'hidden');
-		}
-	}else{
+		//}
+	/*}else{
 		this.total = total;
-	}
-	
-	
-	
+	}*/
+
+
+
 	this.nextButton = next;
 	this.prevButton = prev;
 	var self = this;
 	$(this.prevButton).click(function(){
 		self.prev();
 	}).css('visibility', 'hidden');
-	
+
 	$(this.nextButton).click(function(){
 		self.next();
 	}).css('visibility', 'hidden');
-	
+
 	// this.options = defaultOptions;
 	this.init = function(){
-		this.contentWidth = $(this.content).width();
-		this.containerWidth = $(this.container).width();
+		this.contentWidth = $(this.content).innerWidth();
+		this.containerWidth = $(this.container).innerWidth();
 		if(this.contentWidth && this.containerWidth){
-		
+
 			if(!this.stepWidth) this.stepWidth = this.contentWidth/this.total;
-			
+
 			// Math.round
 			// if(this.contentWidth > this.containerWidth){
 				// $(this.nextButton).css('visibility', 'visible');
 			// }
 			this.updateButtonVisibility();
-			
+
 		}
-	}	
-	
+	}
+
 	this.prev = function(){
 		if(this.current>0){
 			this.current--;
 			this.updateButtonVisibility();
-			
+
 			if(this.stepWidth){
 				var currentMarginLeft = parseInt($(this.content).css('margin-left'));
 				if(!currentMarginLeft) currentMarginLeft = 0;
 				var newMarginLeft = currentMarginLeft + this.stepWidth;
 				$(this.content).css('margin-left', parseInt(newMarginLeft)+'px');
 			}
-			
+
 		}
 	}
-	
+
 	this.next = function(){
 		if(this.current<this.total){
 			this.current++;
 			this.updateButtonVisibility();
-			
+
 			if(this.stepWidth){
 				var currentMarginLeft = parseInt($(this.content).css('margin-left'));
 				if(!currentMarginLeft) currentMarginLeft = 0;
 				var newMarginLeft = currentMarginLeft - this.stepWidth;
 				$(this.content).css('margin-left', parseInt(newMarginLeft)+'px');
 			}
-			
+
 		}
 	}
-	
+
 	this.updateButtonVisibility = function(){
-		
-		var extraSteps = ($(this.content).width()-$(this.container).width())/this.stepWidth;
-				
+
+		var extraSteps = ($(this.content).innerWidth()-$(this.container).innerWidth())/this.stepWidth;
+
 		if(this.current >= extraSteps){
 			$(this.nextButton).css('visibility', 'hidden');
 		}else{
 			$(this.nextButton).css('visibility', 'visible');
 		}
-		
+
 		if(this.current <= 0){
 			$(this.prevButton).css('visibility', 'hidden');
 		}else{
 			$(this.prevButton).css('visibility', 'visible');
 		}
 	}
-	
+
 	this.init();
 	tinyCarousel.instances[this.container] = this;
 }
