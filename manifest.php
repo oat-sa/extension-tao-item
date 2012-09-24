af53c05111552529657770d90038856081a52715
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-09-24 13:08:54 +0000

    added trailing slash to base_url
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoItems/trunk@12258 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -47,7 +47,7 @@ return array(
 		'BASE_PATH'				=> $extpath,
 	
 		#BASE URL (usually the domain root)
-		'BASE_URL'				=> ROOT_URL	.'taoItems',
+		'BASE_URL'				=> ROOT_URL	.'taoItems/',
 	
 		#BASE WWW the web resources path
 		'BASE_WWW'				=> ROOT_URL	.'taoItems/views/',
