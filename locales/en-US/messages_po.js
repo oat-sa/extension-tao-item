a904abd4d11f7b97d9aa564c3897ce76f48324b1
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-05 15:05:27 +0000

    changed description (ran taoTranslate.php -v -a compile -l en-US -e taoItems and update )
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoItems/trunk@20057 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/en-US/messages_po.js
+++ locales/en-US/messages_po.js
@@ -3,6 +3,6 @@
 define(function(){
  return {
    langCode : 'en-US',
-   i18n_tr : []
+   i18n_tr : {"The Items module enables the creation and design of items and exercises.":"You can create and design questions and exercises using different templates provided here, such as multiple choice questions, puzzle design, and text with gaps, etc.\u0000"}
  };
 });
