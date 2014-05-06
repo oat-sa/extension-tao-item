08d6de565a8b99cafb80697fe4f5ace650dae16c
chazotte <chazotte@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-06 08:08:52 +0000

    globally change test taker wording to test-taker; localization should not be broken
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoItems/trunk@20079 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/en-US/messages_po.js
+++ locales/en-US/messages_po.js
@@ -3,6 +3,6 @@
 define(function(){
  return {
    langCode : 'en-US',
-   i18n_tr : {"The Items module enables the creation and design of items and exercises.":"You can create and design questions and exercises using different templates provided here, such as multiple choice questions, puzzle design, and text with gaps, etc.\u0000"}
+   i18n_tr : {"Group test takers according to global features and classifications.":"Group test-takers according to global features and classifications.","Manage test takers":"Manage test-takers","Record and manage test takers.":"Record and manage test-takers.","Test takers":"Test-takers","The Items module enables the creation and design of items and exercises.":"You can create and design questions and exercises using different templates provided here, such as multiple choice questions, puzzle design, and text with gaps, etc.\u0000"}
  };
 });
