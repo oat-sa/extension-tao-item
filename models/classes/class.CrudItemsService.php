8264cf2cbf6397e7a50ecd9004b7938e7367429f
Lionel Lecaque <lionel@taotesting.com>
2014-09-08 18:15:09 +0200

    change crudservice signature

--- models/classes/class.CrudItemsService.php
+++ models/classes/class.CrudItemsService.php
@@ -1,5 +1,6 @@
 <?php
-/*  
+
+/**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -14,70 +15,71 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * 
+ * Copyright (c) 2013-2014 (original work) Open Assessment Technologies SA
  * 
  */
 
 /**
- * .Crud services implements basic CRUD services, orginally intended for REST controllers/ HTTP exception handlers
- *  Consequently the signatures and behaviors is closer to REST and throwing HTTP like exceptions
- *  
+ * Crud services implements basic CRUD services, orginally intended for
+ * REST controllers/ HTTP exception handlers.
+ * Consequently the signatures and behaviors is closer to REST and throwing HTTP like exceptions
  *
- * 
+ * @author Patrick Plichart, patrick@taotesting.com
+ *        
  */
-class taoItems_models_classes_CrudItemsService
-    extends tao_models_classes_CrudService
+class taoItems_models_classes_CrudItemsService extends tao_models_classes_CrudService
 {
-   protected $itemClass = null;
-   
-   protected $itemsServices = null;
-
-    public function __construct(){
-		parent::__construct();
-		$this->itemClass = new core_kernel_classes_Class(TAO_ITEM_CLASS);
-		$this->itemsServices = taoItems_models_classes_ItemsService::singleton();
-    }
-
-    public function getRootClass(){
-		return $this->itemClass;
-	}
-
-    
-    public function delete( $resource){
-         taoItems_models_classes_ItemsService::singleton()->deleteItem(new core_kernel_classes_Resource($resource));
-	//parent::delete($resource)
-         return true;
-    }
-
 
     /**
-     * @param array parameters an array of property uri and values
+     * (non-PHPdoc)
+     * 
+     * @see tao_models_classes_CrudService::getClassService()
      */
-    public function createFromArray(array $propertiesValues){
-	
-	    if (!isset($propertiesValues[RDFS_LABEL])) {
-			$propertiesValues[RDFS_LABEL] = "";
-		}
-		
-		/*if (isset($propertiesValues[TAO_ITEM_CONTENT_PROPERTY]))*/
-		$type = isset($propertiesValues[RDF_TYPE]) ? $propertiesValues[RDF_TYPE] : $this->getRootClass();
-		$label = $propertiesValues[RDFS_LABEL];
-		unset($propertiesValues[RDFS_LABEL]);
-		unset($propertiesValues[RDF_TYPE]);
-
-		$itemContent = null;
-		if (isset($propertiesValues[TAO_ITEM_CONTENT_PROPERTY])) {
-		    $itemContent = $propertiesValues[TAO_ITEM_CONTENT_PROPERTY];
-		    unset($propertiesValues[TAO_ITEM_CONTENT_PROPERTY]);
-		}
-		$resource =  parent::create($label, $type, $propertiesValues);
-		if (isset($itemContent)) {
-		    $this->itemsServices->setItemContent($resource, $itemContent);
-		}
-		return $resource;
+    public function getClassService()
+    {
+        return taoItems_models_classes_ItemsService::singleton();
     }
 
+    /**
+     * (non-PHPdoc)
+     * 
+     * @see tao_models_classes_CrudService::delete()
+     */
+    public function delete($resource)
+    {
+        taoItems_models_classes_ItemsService::singleton()->deleteItem(new core_kernel_classes_Resource($resource));
+        // parent::delete($resource)
+        return true;
+    }
 
-    
-} /* end of class taoItems_models_classes_ItemsService */
+    /**
+     *
+     * @param array parameters an array of property uri and values
+     * @author Patrick Plichart, patrick@taotesting.com
+     */
+    public function createFromArray(array $propertiesValues)
+    {
+        if (! isset($propertiesValues[RDFS_LABEL])) {
+            $propertiesValues[RDFS_LABEL] = "";
+        }
+        
+        /* if (isset($propertiesValues[TAO_ITEM_CONTENT_PROPERTY])) */
+        $type = isset($propertiesValues[RDF_TYPE]) ? $propertiesValues[RDF_TYPE] : $this->getRootClass();
+        $label = $propertiesValues[RDFS_LABEL];
+        unset($propertiesValues[RDFS_LABEL]);
+        unset($propertiesValues[RDF_TYPE]);
+        
+        $itemContent = null;
+        if (isset($propertiesValues[TAO_ITEM_CONTENT_PROPERTY])) {
+            $itemContent = $propertiesValues[TAO_ITEM_CONTENT_PROPERTY];
+            unset($propertiesValues[TAO_ITEM_CONTENT_PROPERTY]);
+        }
+        $resource = parent::create($label, $type, $propertiesValues);
+        if (isset($itemContent)) {
+            $this->itemsServices->setItemContent($resource, $itemContent);
+        }
+        return $resource;
+    }
+}
 
 ?>
