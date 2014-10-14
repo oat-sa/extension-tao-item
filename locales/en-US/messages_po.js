771088a0d29f86cc320f3158830083cfae62477f
Lionel Lecaque <lionel@taotesting.com>
2014-10-14 13:21:38 +0200

    update locales

--- locales/en-US/messages_po.js
+++ locales/en-US/messages_po.js
@@ -1,1 +1,1 @@
-{"Group test takers according to global features and classifications.":"Group test-takers according to global features and classifications.","Manage test takers":"Manage test-takers","Record and manage test takers.":"Record and manage test-takers.","The Items module enables the creation and design of items and exercises.":"You can create and design questions and exercises using different templates provided here, such as multiple choice questions, puzzle design, and text with gaps, etc.\u0000"}
+[]
