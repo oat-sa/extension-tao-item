b43985a80e9b3e15f3cdbb4c805201f4ae903282
antoinerobin <antoine.robin@vesperiagroup.com>
2015-06-17 14:02:07 +0200

    correct documentation

--- test/media/LocalItemSourceTest.php
+++ test/media/LocalItemSourceTest.php
@@ -1,34 +1,31 @@
 <?php
-/*
+/**
  * This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; under version 2
-* of the License (non-upgradable).
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*
-* Copyright (c) 2008-2010 (original work) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
-*               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
-*
-*/
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2015 (original work) Open Assessment Technologies SA;
+ *
+ *
+ */
 use oat\tao\test\TaoPhpUnitTestRunner;
 use oat\taoItems\model\media\LocalItemSource;
 use oat\tao\model\media\MediaAsset;
-//include_once dirname(__FILE__) . '/../includes/raw_start.php';
 
 /**
- * This class aims at testing tao_helpers_Xhtml.
+ * This class aims at testing LocalItemSource.
  *
- * @author Jerome Bogaerts, <jerome@taotesting.com>
  * @package taoItems
- 
  */
 class LocalItemSourceTest extends TaoPhpUnitTestRunner {
     
