40f0b7887d88f66c4ef96289b11ddfab38767c1a
Lionel Lecaque <lionel@taotesting.com>
2015-07-28 14:55:39 +0200

    update version to 2.8

--- scripts/update/class.Updater.php
+++ scripts/update/class.Updater.php
@@ -102,6 +102,9 @@ class taoItems_scripts_update_Updater extends \common_ext_ExtensionUpdater {
         if ($currentVersion == '2.6.5') {
             $currentVersion = '2.7.0';
         }
+        if ($currentVersion == '2.7.0') {
+            $currentVersion = '2.8';
+        }
         return $currentVersion;
     }
 }
