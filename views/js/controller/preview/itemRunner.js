373c7be35cded98e450fbe639854af5d62c44119
Dieter Raber <dieter@taotesting.com>
2015-10-26 15:36:53 +0100

    adding scope to distinguish preview from test + tiny jsHint

--- views/js/controller/preview/itemRunner.js
+++ views/js/controller/preview/itemRunner.js
@@ -27,7 +27,7 @@ define(
         'serviceApi/UserInfoService',
         'taoItems/runtime/ItemServiceImpl',
         'taoItems/preview/preview-console',
-        'urlParser',
+        'urlParser'
     ],
     function (
         module,
@@ -40,6 +40,8 @@ define(
         previewConsole,
         UrlParser
         ) {
+
+        'use strict';
 
         var previewItemRunner = {
 
@@ -101,6 +103,9 @@ define(
                             //then we can wait a specific event triggered from the item
                             $(document).on('itemready', function () {
                                 itemApi.connect(frame);
+                                if(frame.contentWindow.__knownParent__) {
+                                    frame.contentWindow.document.body.className += ' tao-preview-scope';
+                                }
                             });
                         });
 
