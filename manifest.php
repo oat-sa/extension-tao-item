f8b41fd3433ced0613df803dc4d79244ad31102a
Mikhail Kamarouski <Komarouski@1pt.com>
2016-07-22 12:15:40 +0300

    Version up.

--- manifest.php
+++ manifest.php
@@ -34,7 +34,7 @@ return array(
     'label' => 'Item core extension',
 	'description' => 'TAO Items extension',
     'license' => 'GPL-2.0',
-    'version' => '2.19.0',
+    'version' => '2.20.0',
 	'author' => 'Open Assessment Technologies, CRP Henri Tudor',
 	'requires' => array(
 	    'taoBackOffice' => '>=0.8'
