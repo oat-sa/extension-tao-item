859635b10e75dc93bc2c3b93c456f2413a5a7a24
cmoyon <camille@taotesting.com>
2016-09-22 17:23:30 +0200

    Adpat tokenizer to accept item content tokenizer by model

--- models/classes/search/class.ItemContentTokenizer.php
+++ models/classes/search/class.ItemContentTokenizer.php
@@ -1,5 +1,4 @@
 <?php
-
 /**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -15,48 +14,68 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * 
- * Copyright (c) 2014 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  * 
  */
 
-use oat\tao\model\search\tokenizer\Tokenizer;
+use oat\tao\model\search\tokenizer\ResourceTokenizer;
 
 /**
  * Item content tokenizer.
  *
  * @author Joel Bout <joel@taotesting.com>
  * @author Jérôme Bogaerts <jerome@taotesting.com>
+ * @author Camille Moyon <camille@taotesting.com>
  */
-class taoItems_models_classes_search_ItemContentTokenizer implements Tokenizer
+class taoItems_models_classes_search_ItemContentTokenizer implements ResourceTokenizer
 {
-    public function getStrings($values)
+    use \oat\generis\model\OntologyAwareTrait;
+
+    /**
+     * Find item model tokenizer and send request to it to extract tokens
+     *
+     * @param core_kernel_classes_Resource $resource
+     * @return array
+     */
+    public function getStrings(\core_kernel_classes_Resource $resource)
     {
-        $contentStrings = array();
-        
-        $xmlTokenizer = new taoItems_models_classes_search_XmlItemContentTokenizer();
-        
-        foreach ($values as $valueUri) {
-            $file = new core_kernel_file_File($valueUri);
-            try {
-                $content = file_get_contents($file->getAbsolutePath());
-                if ($content === false) {
-                    common_Logger::w('File '.$file->getAbsolutePath().' not found for item');
-                } else {
-                    // Try to make it a DOM Document...
-                    $dom = new DOMDocument('1.0', 'UTF-8');
-                    
-                    if (@$dom->loadXML($content) === true) {
-                        $contentStrings = array_merge($contentStrings, $xmlTokenizer->getStrings($dom));
-                        unset($dom);
-                    } else {
-                        common_Logger::d('Skipped non XML content for '.$file->getUri());
+        $tokenizer = $this->getItemContentTokenizer($resource);
+        if (is_null($tokenizer)) {
+            return [];
+        }
+        return $tokenizer->getStrings($resource);
+    }
+
+    /**
+     * Get item content tokenizer associated to $resource e.q. item model
+     * If not return null
+     *
+     * @param core_kernel_classes_Resource $resource
+     * @return ResourceTokenizer|null
+     */
+    protected function getItemContentTokenizer(core_kernel_classes_Resource $resource)
+    {
+        try {
+            /** @var core_kernel_classes_Resource $model */
+            $model = $resource->getUniquePropertyValue($this->getProperty(TAO_ITEM_MODEL_PROPERTY));
+            if ($model->exists()) {
+                /** @var core_kernel_classes_Resource $tokenizer */
+                $tokenizer = $model->getUniquePropertyValue($this->getProperty(INDEX_PROPERTY_TOKENIZER));
+                if ($tokenizer->exists()) {
+                    $tokenizerClass = $tokenizer->getUniquePropertyValue($this->getProperty("http://www.tao.lu/Ontologies/TAO.rdf#TokenizerClass"));
+                    if ($tokenizerClass instanceof core_kernel_classes_Literal) {
+                        $impl = new $tokenizerClass->literal();
+                        if ($impl instanceof ResourceTokenizer) {
+                            return $impl;
+                        }
                     }
                 }
-            } catch (common_Exception $exc) {
-                common_Logger::w('Invalid file '.$valueUri.' for ItemContentTokenizer: '.$exc->getMessage());
             }
+
         }
-        
-        return $contentStrings;
+        catch (core_kernel_classes_EmptyProperty $e) {}
+        catch (core_kernel_classes_MultiplePropertyValuesException $e) {}
+
+        return null;
     }
 }
