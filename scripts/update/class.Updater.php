56433d8dd40310c0c68a87c0b51650c57cae3bb0
Sam Sipasseuth <sam@taotesting.com>
2017-05-11 15:14:01 +0200

    bump version

--- scripts/update/class.Updater.php
+++ scripts/update/class.Updater.php
@@ -139,6 +139,6 @@ class taoItems_scripts_update_Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('2.24.0');
         }
 
-        $this->skip('2.24.0', '4.2.0');
+        $this->skip('2.24.0', '4.3.0');
     }
 }
