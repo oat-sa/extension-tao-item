57cf9c99d5fd84eedc99ce50ba9c732a21893d6a
jsconan <jsconan@gmail.com>
2017-05-11 09:48:31 +0200

    Bump version

--- manifest.php
+++ manifest.php
@@ -35,12 +35,12 @@ return array(
     'label' => 'Item core extension',
     'description' => 'TAO Items extension',
     'license' => 'GPL-2.0',
-    'version' => '4.2.0',
+    'version' => '4.2.1',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'taoBackOffice' => '>=0.8',
         'generis' => '>=3.14.0',
-        'tao' => '>=9.0.0'
+        'tao' => '>=10.1.0'
     ),
     'models' => array(
 		'http://www.tao.lu/Ontologies/TAOItem.rdf'
