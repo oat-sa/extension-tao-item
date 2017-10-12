e6c265622107beda6b6b85f44a2a9b076db4cbf6
antoine <antoine@taotesting.com>
2017-10-12 14:33:09 +0200

    update dependency and version number

--- manifest.php
+++ manifest.php
@@ -35,12 +35,12 @@ return array(
     'label' => 'Item core extension',
     'description' => 'TAO Items extension',
     'license' => 'GPL-2.0',
-    'version' => '5.1.1',
+    'version' => '5.2.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'taoBackOffice' => '>=0.8',
         'generis' => '>=3.14.0',
-        'tao' => '>=12.7.0'
+        'tao' => '>=12.21.0'
     ),
     'models' => array(
 		'http://www.tao.lu/Ontologies/TAOItem.rdf'
