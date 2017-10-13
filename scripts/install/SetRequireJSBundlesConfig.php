a175bbb2b007fc88ad103684dc7acea9da2861b0
eric <eacraft13@gmail.com>
2017-10-13 12:11:10 -0500

    Bump major version and add install/update scripts
    tao-4814

---
+++ scripts/install/SetRequireJSBundlesConfig.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ */
+
+namespace oat\taoItems\scripts\install;
+
+use oat\oatbox\extension\InstallAction;
+
+/**
+ * This declares bundling options for requirejs to the extensions configuration
+ */
+class SetRequireJSBundlesConfig extends InstallAction
+{
+    public function __invoke($params)
+    {
+        $ext = common_ext_ExtensionsManager::singleton()->getExtensionById('taoItems');
+        $ext->setConfig('requirejsbundles', array(
+            array(
+                'name' => 'taoitemsbundle',
+                'path' => ROOT_URL . 'taoItems/views/dist/loader/controllers.min',
+                'modules' => array(
+                    'taoItems/controller/items/action',
+                    'taoItems/controller/items/edit',
+                    'taoItems/controller/items/editItemClass',
+                    'taoItems/controller/preview/itemRunner',
+                    'taoItems/controller/routes',
+                    'taoItems/controller/runtime/itemRunner'
+                ),
+            ),
+        ));
+    }
+}
