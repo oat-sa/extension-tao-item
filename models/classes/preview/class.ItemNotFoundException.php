bb1feaf20471accaa02605758da7999054269739
Ilya Yarkavets <ilya.yarkavets@1pt.com>
2017-10-25 18:34:18 +0300

    Changed parent class from common_Exception to Exception

--- models/classes/preview/class.ItemNotFoundException.php
+++ models/classes/preview/class.ItemNotFoundException.php
@@ -1,6 +1,6 @@
 <?php
 
-class taoItems_models_classes_preview_ItemNotFoundException extends common_Exception implements common_exception_UserReadableException
+class taoItems_models_classes_preview_ItemNotFoundException extends Exception implements common_exception_UserReadableException
 {
 
     public function getUserMessage()
