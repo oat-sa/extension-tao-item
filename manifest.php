fe33f44136b3521f76f636b202ab82ef0b224587
Ionut Padurariu <ionut@taotesting.com>
2017-12-11 16:09:49 +0100

    increase version

--- manifest.php
+++ manifest.php
@@ -35,12 +35,12 @@ return array(
     'label' => 'Item core extension',
     'description' => 'TAO Items extension',
     'license' => 'GPL-2.0',
-    'version' => '5.2.1',
+    'version' => '5.3.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'taoBackOffice' => '>=0.8',
-        'generis' => '>=3.14.0',
-        'tao' => '>=12.21.0'
+        'generis' => '>=5.9.0',
+        'tao' => '>=14.3.1'
     ),
     'models' => array(
 		'http://www.tao.lu/Ontologies/TAOItem.rdf'
