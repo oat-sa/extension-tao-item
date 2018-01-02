810dbd11af6bbd17261aec335e21cdbe642e6cbc
Gyula Szucs <gyula@taotesting.com>
2018-01-02 15:33:30 +0100

    TAO-4578 use task queue for item import through web ui

--- actions/class.ItemImport.php
+++ actions/class.ItemImport.php
@@ -19,54 +19,89 @@
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
  */
+
 use oat\taoItems\model\event\ItemImportEvent;
+use oat\taoItems\model\task\ImportItemByHandler;
+use oat\taoQtiItem\model\import\QtiItemImport;
+use oat\taoQtiItem\model\import\QtiPackageImport;
+use oat\taoTaskQueue\model\TaskLogActionTrait;
 
 /**
- * This controller provide the actions to import items 
- * 
- * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
+ * This controller provide the actions to import items
+ *
+ * @author  CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  * @package taoItems
- 
- *
  */
-class taoItems_actions_ItemImport extends tao_actions_Import {
-	
+class taoItems_actions_ItemImport extends tao_actions_Import
+{
+    use TaskLogActionTrait;
+
     /**
      * overwrite the parent index to add the requiresRight for Items only
-     * 
+     *
      * @requiresRight id WRITE
-     * @see tao_actions_Import::index()
+     * @see           tao_actions_Import::index()
      */
     public function index()
     {
         parent::index();
     }
 
-	protected function getAvailableImportHandlers() {
-		$returnValue = parent::getAvailableImportHandlers();
+    /**
+     * Uses task queue for only QTI import.
+     *
+     * @param tao_models_classes_import_ImportHandler $importer
+     * @param tao_helpers_form_Form                   $importForm
+     * @return string
+     * @throws Exception
+     * @throws common_exception_NotAcceptable
+     */
+    protected function handleSubmittedForm(tao_models_classes_import_ImportHandler $importer, tao_helpers_form_Form $importForm)
+    {
+        // use task for only QTI import
+        if ($importer instanceof QtiPackageImport || $importer instanceof QtiItemImport) {
+            if (!\tao_helpers_Request::isAjax()) {
+                throw new \Exception('Only ajax call allowed.');
+            }
 
-		foreach (array_keys($returnValue) as $key) {
-			if ($returnValue[$key] instanceof \tao_models_classes_import_CsvImporter) {
-				$importer =  new \oat\taoItems\model\CsvImporter();
-				$returnValue[$key] = $importer;
-			}
-		}
+            $task = ImportItemByHandler::createTask($importer, $importForm, $this->getCurrentClass());
 
-		$itemModelClass = new core_kernel_classes_Class(taoItems_models_classes_itemModel::CLASS_URI_MODELS);
-		foreach ($itemModelClass->getInstances() as $model) {
-			$impl = taoItems_models_classes_ItemsService::singleton()->getItemModelImplementation($model);
-			if (in_array('tao_models_classes_import_ImportProvider', class_implements($impl))) {
-				foreach ($impl->getImportHandlers() as $handler) {
-					array_unshift($returnValue, $handler);
-				}
-			}
-		}
+            return $this->returnTaskJson($task);
+        } else {
+            return parent::handleSubmittedForm($importer, $importForm);
+        }
+    }
+
+    /**
+     * @return array
+     * @throws common_exception_Error
+     * @throws common_exception_NoImplementation
+     */
+    protected function getAvailableImportHandlers()
+    {
+        $returnValue = parent::getAvailableImportHandlers();
+
+        foreach (array_keys($returnValue) as $key) {
+            if ($returnValue[$key] instanceof \tao_models_classes_import_CsvImporter) {
+                $importer = new \oat\taoItems\model\CsvImporter();
+                $returnValue[$key] = $importer;
+            }
+        }
+
+        $itemModelClass = new core_kernel_classes_Class(taoItems_models_classes_itemModel::CLASS_URI_MODELS);
+        foreach ($itemModelClass->getInstances() as $model) {
+            $impl = taoItems_models_classes_ItemsService::singleton()->getItemModelImplementation($model);
+            if (in_array('tao_models_classes_import_ImportProvider', class_implements($impl))) {
+                foreach ($impl->getImportHandlers() as $handler) {
+                    array_unshift($returnValue, $handler);
+                }
+            }
+        }
 
 
-
-		return $returnValue;
-	}
+        return $returnValue;
+    }
 
     /**
      * @inheritdoc
---
+++ models/classes/task/ImportItemByHandler.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ */
+
+namespace oat\taoItems\model\task;
+
+use common_report_Report as Report;
+use oat\oatbox\event\EventManagerAwareTrait;
+use oat\oatbox\extension\AbstractAction;
+use oat\oatbox\service\ServiceManager;
+use oat\tao\model\upload\UploadService;
+use oat\taoItems\model\event\ItemImportEvent;
+use oat\taoTaskQueue\model\QueueDispatcher;
+use oat\taoTaskQueue\model\Task\CallbackTaskInterface;
+
+/**
+ * Import item by a tao_models_classes_import_ImportHandler
+ *
+ * @author Gyula Szucs <gyula@taotesting.com>
+ */
+class ImportItemByHandler extends AbstractAction
+{
+    use EventManagerAwareTrait;
+
+    const PARAM_IMPORT_HANDLER = 'import_handler';
+    const PARAM_FORM_VALUES = 'form_values';
+    const PARAM_PARENT_CLASS = 'parent_class_uri';
+
+    /**
+     * @param array $params
+     * @return \common_report_Report
+     */
+    public function __invoke($params)
+    {
+        if (!isset($params[self::PARAM_IMPORT_HANDLER]) || !class_exists($params[self::PARAM_IMPORT_HANDLER])) {
+            throw new \InvalidArgumentException('Please provide a valid import handler');
+        }
+
+        /** @var \tao_models_classes_import_ImportHandler $importer */
+        $importer = new $params[self::PARAM_IMPORT_HANDLER];
+
+        /** @var Report $report */
+        $report = $importer->import(new \core_kernel_classes_Class($params[self::PARAM_PARENT_CLASS]), $params[self::PARAM_FORM_VALUES]);
+
+        $this->onAfterImport($report);
+
+        return $report;
+    }
+
+    /**
+     * @param Report $report
+     */
+    protected function onAfterImport(Report $report)
+    {
+        if (Report::TYPE_SUCCESS == $report->getType()) {
+            $this->getEventManager()->trigger(new ItemImportEvent($report));
+        }
+    }
+
+    /**
+     * @param \tao_models_classes_import_ImportHandler $importer
+     * @param \tao_helpers_form_Form                   $importForm
+     * @param \core_kernel_classes_Class               $parentClass
+     * @return CallbackTaskInterface
+     * @throws \common_exception_NotAcceptable
+     */
+    public static function createTask(\tao_models_classes_import_ImportHandler $importer, \tao_helpers_form_Form $importForm, \core_kernel_classes_Class $parentClass)
+    {
+        $action = new static();
+        $action->setServiceLocator(ServiceManager::getServiceManager());
+
+        /** @var  UploadService $uploadService */
+        $uploadService = ServiceManager::getServiceManager()->get(UploadService::SERVICE_ID);
+        $file = $uploadService->getUploadedFlyFile($importForm->getValue('source')['uploaded_file']);
+
+        $formValues = [
+            'fly_file' => $file->getPrefix(),
+        ];
+
+        if ($importForm->hasElement('rollback')) {
+            $formValues['rollback'] = $importForm->getValue('rollback');
+        }
+
+        /** @var QueueDispatcher $queueDispatcher */
+        $queueDispatcher = ServiceManager::getServiceManager()->get(QueueDispatcher::SERVICE_ID);
+
+        return $queueDispatcher->createTask(
+            $action,
+            [
+                self::PARAM_IMPORT_HANDLER => get_class($importer),
+                self::PARAM_FORM_VALUES => $formValues,
+                self::PARAM_PARENT_CLASS => $parentClass->getUri()
+            ],
+            __('Import a "%s" into "%s"', $importer->getLabel(), $parentClass->getLabel())
+        );
+    }
+}
