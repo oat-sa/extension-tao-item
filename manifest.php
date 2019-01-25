1dfdfa315978d94f9423efc451b2993bd4c93519
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2019-01-25 11:18:40 +0100

    update tao-core dependency

--- manifest.php
+++ manifest.php
@@ -40,7 +40,7 @@ return array(
     'requires' => array(
         'taoBackOffice' => '>=3.0.0',
         'generis' => '>=8.0.0',
-        'tao' => '>=23.0.0'
+        'tao' => '>=24.0.0'
     ),
     'models' => array(
 		'http://www.tao.lu/Ontologies/TAOItem.rdf'
