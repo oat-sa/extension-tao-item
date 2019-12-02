bc7d0aa07e54a50bebc9a7fceb92b170ef5be28f
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2019-12-02 16:04:51 +0100

    version bump

--- manifest.php
+++ manifest.php
@@ -35,12 +35,12 @@ return array(
     'label' => 'Item core extension',
     'description' => 'TAO Items extension',
     'license' => 'GPL-2.0',
-    'version' => '10.0.1',
+    'version' => '10.1.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'taoBackOffice' => '>=3.0.0',
         'generis' => '>=12.5.0',
-        'tao' => '>=38.5.0'
+        'tao' => '>=39.7.0'
     ),
     'models' => array(
 		'http://www.tao.lu/Ontologies/TAOItem.rdf'
