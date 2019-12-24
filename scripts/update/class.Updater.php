e33eef665e48a54b747d0406573c16a9bc74816f
lecosson <lecosson@gmail.com>
2019-12-24 13:14:50 +0300

    add getApipData to itemRunner - update version                                                                                                                              https://oat-sa.atlassian.net/browse/TAO-9637 ([ACT] [TTS - FE] Acces the data about APIP the content and related media files)

--- scripts/update/class.Updater.php
+++ scripts/update/class.Updater.php
@@ -112,7 +112,7 @@ class taoItems_scripts_update_Updater extends \common_ext_ExtensionUpdater {
             $clientLibRegistry = ClientLibRegistry::getRegistry();
             $clientLibRegistry->register('taoItems/assets', $taoItemsNpmDist . 'assets');
             $clientLibRegistry->register('taoItems/runner', $taoItemsNpmDist . 'runner');
-            $clientLibRegistry->register('taoItems/scoring', $taoItemsNpmDist . 'scoring');    
+            $clientLibRegistry->register('taoItems/scoring', $taoItemsNpmDist . 'scoring');
             $this->setVersion('9.0.0');
         }
 
