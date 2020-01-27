1431c3d733db1066786e0a783b750b90166296c5
Camille Moyon <camille@taotesting.com>
2020-01-27 16:47:38 +0100

    Bump version

--- manifest.php
+++ manifest.php
@@ -37,7 +37,7 @@ return [
     'label' => 'Item core extension',
     'description' => 'TAO Items extension',
     'license' => 'GPL-2.0',
-    'version' => '10.1.2',
+    'version' => '10.1.3',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'taoBackOffice' => '>=3.0.0',
