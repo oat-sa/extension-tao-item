c9eb8e703964ee88879ea5fb14f017c7ff0d267e
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2020-05-25 17:19:28 +0200

    BSA-122 fix: expose only eligible item properties as test-item categories

--- models/classes/CategoryService.php
+++ models/classes/CategoryService.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2016-2017 (original work) Open Assessment Technologies SA
+ * Copyright (c) 2016-2020 (original work) Open Assessment Technologies SA
  */
 
 namespace oat\taoItems\model;
@@ -23,9 +23,9 @@ namespace oat\taoItems\model;
 use core_kernel_classes_Class as RdfClass;
 use core_kernel_classes_Property as RdfProperty;
 use core_kernel_classes_Resource as RdfResource;
+use oat\generis\model\GenerisRdf;
 use oat\oatbox\service\ConfigurableService;
 use taoItems_models_classes_ItemsService;
-use oat\generis\model\GenerisRdf;
 
 /**
  * Category management service.
@@ -132,19 +132,25 @@ class CategoryService extends ConfigurableService
      *
      * @param RdfClass $class the $class
      *
-     * @return RdfProperties[] the list of eligible properties
+     * @return RdfProperty[] the list of eligible properties
      */
     public function getElligibleProperties(RdfClass $class)
     {
         $properties = $this->getItemService()->getClazzProperties($class, new RdfClass(self::ITEM_CLASS_URI));
+
         return array_filter(
             $properties,
-            function ($property) {
-                if (in_array($property->getUri(), self::$excludedPropUris)) {
+            function (RdfProperty $property) {
+                if (
+                    in_array($property->getUri(), self::$excludedPropUris, true)
+                    || !$this->doesExposeCategory($property)
+                ) {
                     return false;
                 }
+
                 $widget = $property->getWidget();
-                return !is_null($widget) && in_array($widget->getUri(), self::$supportedWidgetUris);
+
+                return null !== $widget && in_array($widget->getUri(), self::$supportedWidgetUris, true);
             }
         );
     }
@@ -152,7 +158,7 @@ class CategoryService extends ConfigurableService
     /**
      * Check if a property is exposed
      *
-     * @param RdfPropery $property the property to check
+     * @param RdfProperty $property the property to check
      *
      * @return bool true if exposed
      */
