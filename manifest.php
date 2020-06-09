8edd85e4dd7b0a465faf78b24e9955613ffbce58
gitromba <mihaly.hajdu@taotesting.com>
2020-06-09 19:33:48 +0200

    chore: bump manifest version

--- manifest.php
+++ manifest.php
@@ -38,7 +38,7 @@ return [
     'label' => 'Item core extension',
     'description' => 'TAO Items extension',
     'license' => 'GPL-2.0',
-    'version' => '10.6.2',
+    'version' => '10.7.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'taoBackOffice' => '>=3.0.0',
