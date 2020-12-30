0a6c0637dc64b394093cf3945c1dacc59d876845
Hanna Dzmitryieva <bziondik@gmail.com>
2020-12-30 11:06:28 +0300

    chore: bump version

--- manifest.php
+++ manifest.php
@@ -40,7 +40,7 @@ return [
     'label' => 'Item core extension',
     'description' => 'TAO Items extension',
     'license' => 'GPL-2.0',
-    'version' => '10.18.2',
+    'version' => '10.19.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'taoBackOffice' => '>=3.0.0',
