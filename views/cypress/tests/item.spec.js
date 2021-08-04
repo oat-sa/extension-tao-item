dadce1d57c5ba06a144c2c9eb98cd932c435829b
Juan Luis Gutierrez Dos Santos <juanluis.gutierrezdossantos@taotesting.com>
2021-08-04 16:15:23 +0200

    chore: Push my local changes

--- views/cypress/tests/item.spec.js
+++ views/cypress/tests/item.spec.js
@@ -16,89 +16,127 @@
  * Copyright (c) 2021 (original work) Open Assessment Technologies SA ;
  */
 
-import urls from '../utils/urls';
-import selectors from '../utils/selectors';
+/**
+ * Commands
+ */
+Cypress.Commands.add('addClass', formSelector => {
+    cy.log('COMMAND: addClass');
+    cy.get('[data-context=resource][data-action=subClass]').click();
+    cy.get(formSelector).should('exist');
+});
 
+Cypress.Commands.add('addClassToRoot', (rootSelector, formSelector, name) => {
+    cy.log('COMMAND: addClassToRoot', name);
 
-describe('Items', () => {
-    const className = 'Test E2E class';
-    const classMovedName = 'Test E2E class Moved';
+    cy.get(`${rootSelector} a`)
+        .first()
+        .click();
 
-    /**
-     * Visit the page
-     */
-    beforeEach(() => {
-        cy.visit(urls.items);
+    cy.addClass(formSelector).then(() => {
+        cy.renameSelected(formSelector, name);
     });
+});
 
-    /**
-     * Log in
-     */
-    before(() => {
-        cy.loginAsAdmin();
-    });
+Cypress.Commands.add('moveClass', (formSelector, moveSelector, moveConfirmSelector, name, nameWhereMove) => {
+    cy.log('COMMAND: moveClass', name);
 
-    /**
-     * Tests
-     */
-    describe('Item creation, editing and deletion', () => {
-        it('can create a new item class', function () {
-            cy.addClassToRoot(selectors.root, selectors.itemClassForm, className);
-        });
-
-        it('can create and rename a new item', function () {
-            cy.selectNode(selectors.root, selectors.itemClassForm, className);
-            cy.addNode(selectors.itemForm, selectors.addItem);
-            cy.renameSelected(selectors.itemForm, 'Test E2E item 1');
-        });
-
-        it('can delete item', function () {
-            cy.selectNode(selectors.root, selectors.itemClassForm, className);
-            cy.addNode(selectors.itemForm, selectors.addItem);
-            cy.renameSelected(selectors.itemForm, 'Test E2E item 2');
-            cy.deleteNode(selectors.deleteItem, 'Test E2E item 2');
-        });
-
-        it('can delete item class', function () {
-            cy.deleteClassFromRoot(
-                selectors.root,
-                selectors.itemClassForm,
-                selectors.deleteClass,
-                selectors.deleteConfirm,
-                className
-            );
-        });
-
-        it('can delete empty item class', function () {
-            cy.addClassToRoot(selectors.root, selectors.itemClassForm, className);
-            cy.deleteClassFromRoot(
-                selectors.root,
-                selectors.itemClassForm,
-                selectors.deleteClass,
-                selectors.deleteConfirm,
-                className
-            );
-        });
-
-        it('can move item class', function () {
-            cy.moveClassFromRoot(
-                selectors.root,
-                selectors.itemClassForm,
-                selectors.moveClass,
-                selectors.moveConfirmSelector,
-                className,
-                classMovedName
-            );
+    cy.get(`li[title="${name}"] a`).first().click().then(() => {
+        cy.get(moveSelector).click();
+        cy.get('.destination-selector')
+            .find(`a[title="${nameWhereMove}"]`).then(() => {
+            cy.get(`a[title="${nameWhereMove}"]`).click();
+            cy.get('.actions button').click();
+            cy.get(moveConfirmSelector).click();
+            cy.get(`li[title="${name}"] a`).should('not.exist');
         });
     });
+});
 
-    after(() => {
-        cy.deleteClassFromRoot(
-            selectors.root,
-            selectors.assetClassForm,
-            selectors.deleteClass,
-            selectors.deleteConfirm,
-            classMovedName
-        );
+Cypress.Commands.add('moveClassFromRoot', (rootSelector, formSelector, moveSelector, moveConfirmSelector, name, nameWhereMove) => {
+    cy.log('COMMAND: moveClassFromRoot', name);
+
+    cy.addClassToRoot(rootSelector, formSelector, name);
+    cy.addClassToRoot(rootSelector, formSelector, nameWhereMove);
+
+    cy.get(`${rootSelector} a`)
+        .first()
+        .click();
+
+    cy.get(rootSelector)
+        .find(`li[title="${name}"] a`).then(() => {
+        cy.moveClass(formSelector, moveSelector, moveConfirmSelector, name, nameWhereMove);
     });
+});
+
+Cypress.Commands.add('deleteClass', (formSelector, deleteSelector, confirmSelector, name) => {
+    cy.log('COMMAND: deleteClass', name);
+
+    // cy.contains('a', name).click();
+    cy.get(formSelector)
+        .should('exist')
+        .get(deleteSelector)
+        .click()
+        .get('.modal-body label[for=confirm]')
+        .click()
+        .get(confirmSelector)
+        .click();
+});
+
+Cypress.Commands.add('deleteClassFromRoot', (rootSelector, formSelector, deleteSelector, confirmSelector, name) => {
+    cy.log('COMMAND: deleteClassFromRoot', name);
+
+    cy.get(`${rootSelector} a`)
+        .first()
+        .click()
+        .get(`li[title="${name}"] a`).then(() => {
+        cy.deleteClass(formSelector, deleteSelector, confirmSelector, name);
+    });
+});
+
+Cypress.Commands.add('deleteEmptyClassFromRoot', (rootSelector, formSelector, deleteSelector, confirmSelector, name) => {
+    cy.log('COMMAND: deleteEmptyClassFromRoot', name);
+
+    cy.addClassToRoot(rootSelector, formSelector, name);
+    cy.deleteClassFromRoot(rootSelector, formSelector, deleteSelector, confirmSelector, name);
+});
+
+Cypress.Commands.add('addNode', (formSelector, addSelector) => {
+    cy.log('COMMAND: addNode');
+
+    cy.get(addSelector).click();
+    cy.get(formSelector).should('exist');
+});
+
+Cypress.Commands.add('selectNode', (rootSelector, formSelector, name) => {
+    cy.log('COMMAND: selectNode', name);
+
+    cy.get(`${rootSelector} a`).first().click();
+    cy.contains('a', name).click();
+    cy.get(formSelector).should('exist');
+});
+
+Cypress.Commands.add('deleteNode', (deleteSelector, name) => {
+    cy.log('COMMAND: deleteNode', name);
+
+    cy.contains('a', name).click();
+
+    cy.get(deleteSelector).click();
+    cy.get('[data-control="ok"]').click();
+
+    cy.contains('a', name).should('not.exist');
+});
+
+Cypress.Commands.add('renameSelected', (formSelector, newName) => {
+    cy.log('COMMAND: renameSelectedClass', newName);
+
+    // TODO: update selector when data-testid attributes will be added
+    cy.get(`${ formSelector } input[name*=label]`)
+        .clear()
+        .type(newName)
+        .get('button[id="Save"]')
+        .click();
+
+    cy.get(formSelector).should('exist');
+
+    cy.contains('a', newName).should('exist');
 });
