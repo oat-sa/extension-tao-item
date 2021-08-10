f4576243b6a0c1013c34da192b0a55f8b53858c9
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2021-08-10 17:07:06 +0200

    feat: add item and assets permissions validation

---
+++ migrations/Version202108100944252141_taoItems.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\taoItems\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use oat\taoItems\model\user\TaoItemsRoles;
+use oat\tao\scripts\tools\accessControl\SetRolesAccess;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+
+final class Version202108100944252141_taoItems extends AbstractMigration
+{
+    private const CONFIG = [
+        SetRolesAccess::CONFIG_RULES => [
+            TaoItemsRoles::ITEM_CONTENT_CREATOR => [
+                ['ext' => 'taoItems', 'mod' => 'ItemContent', 'act' => 'delete'],
+                ['ext' => 'taoItems', 'mod' => 'ItemContent', 'act' => 'fileExists'],
+                ['ext' => 'taoItems', 'mod' => 'ItemContent', 'act' => 'upload'],
+            ],
+        ],
+    ];
+
+    public function getDescription(): string
+    {
+        return 'Item content creator role to author existing item';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $setRolesAccess = $this->propagate(new SetRolesAccess());
+        $setRolesAccess(
+            [
+                '--' . SetRolesAccess::OPTION_CONFIG,
+                self::CONFIG,
+            ]
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $setRolesAccess = $this->propagate(new SetRolesAccess());
+        $setRolesAccess([
+            '--' . SetRolesAccess::OPTION_REVOKE,
+            '--' . SetRolesAccess::OPTION_CONFIG, self::CONFIG,
+        ]);
+    }
+}
