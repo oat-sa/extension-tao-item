896f9f3d0a3f970b99fe2d54707b60f52b85f439
Chinnu Francis <chinnu@focaloid.com>
2021-08-25 18:00:04 +0530

    refactor: configure deny for Item Content creator role

---
+++ migrations/Version202108250549242141_taoItems.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\taoItems\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+use oat\tao\scripts\tools\accessControl\SetRolesAccess;
+use oat\taoItems\model\user\TaoItemsRoles;
+use oat\tao\model\accessControl\ActionAccessControl;
+use oat\tao\scripts\update\OntologyUpdater;
+use taoItems_actions_ItemContent;
+
+final class Version202108250549242141_taoItems extends AbstractMigration
+{
+    private const CONFIG = [
+        SetRolesAccess::CONFIG_PERMISSIONS => [
+            taoItems_actions_ItemContent::class => [
+                'upload' => [
+                    TaoItemsRoles::ITEM_CONTENT_CREATOR => ActionAccessControl::DENY,
+                ],
+                'download' => [
+                    TaoItemsRoles::ITEM_CONTENT_CREATOR => ActionAccessControl::DENY,
+                ],
+                'delete' => [
+                    TaoItemsRoles::ITEM_CONTENT_CREATOR => ActionAccessControl::DENY,
+                ],
+            ],
+        ],
+    ];
+
+    public function getDescription(): string
+    {
+        return 'configure deny for insert/delete/download actions for Item Content creator role';
+    }
+
+    public function up(Schema $schema): void
+    {
+        OntologyUpdater::syncModels();
+
+        $setRolesAccess = $this->propagate(new SetRolesAccess());
+        $setRolesAccess([
+            '--' . SetRolesAccess::OPTION_CONFIG, self::CONFIG,
+        ]);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $setRolesAccess = $this->propagate(new SetRolesAccess());
+        $setRolesAccess([
+            '--' . SetRolesAccess::OPTION_REVOKE,
+            '--' . SetRolesAccess::OPTION_CONFIG, self::CONFIG,
+        ]);
+    }
+}
