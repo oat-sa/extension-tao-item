3daab91a288e8a23268026c40b78a1249742b25e
chinnu-focaloid <57711251+chinnu-focaloid@users.noreply.github.com>
2021-09-02 14:42:22 +0530

    Fix/adf 627/item content creator permissions (#517)
    * fix: configure different permission for Item Content creator role

--- migrations/Version202108250549242141_taoItems.php
+++ migrations/Version202108250549242141_taoItems.php
@@ -20,29 +20,20 @@
 
 declare(strict_types=1);
 
-namespace oat\taoItems\scripts\install;
+namespace oat\taoItems\migrations;
 
-use taoItems_actions_Items;
+use Doctrine\DBAL\Schema\Schema;
 use taoItems_actions_ItemContent;
-use oat\oatbox\extension\InstallAction;
 use oat\taoItems\model\user\TaoItemsRoles;
+use oat\tao\scripts\update\OntologyUpdater;
 use oat\tao\model\accessControl\ActionAccessControl;
 use oat\tao\scripts\tools\accessControl\SetRolesAccess;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
 
-class SetRolesPermissions extends InstallAction
+final class Version202108250549242141_taoItems extends AbstractMigration
 {
     private const CONFIG = [
         SetRolesAccess::CONFIG_PERMISSIONS => [
-            taoItems_actions_Items::class => [
-                'editClassLabel' => [
-                    TaoItemsRoles::ITEM_CLASS_NAVIGATOR => ActionAccessControl::READ,
-                    TaoItemsRoles::ITEM_CLASS_EDITOR => ActionAccessControl::WRITE,
-                ],
-                'editItem' => [
-                    TaoItemsRoles::ITEM_VIEWER => ActionAccessControl::READ,
-                    TaoItemsRoles::ITEM_PROPERTIES_EDITOR => ActionAccessControl::WRITE,
-                ],
-            ],
             taoItems_actions_ItemContent::class => [
                 'viewAsset' => [
                     TaoItemsRoles::ITEM_CLASS_NAVIGATOR => ActionAccessControl::DENY,
@@ -63,11 +54,52 @@ class SetRolesPermissions extends InstallAction
         ],
     ];
 
-    public function __invoke($params = [])
+    private const REVOKE_CONFIG = [
+        SetRolesAccess::CONFIG_PERMISSIONS => [
+            taoItems_actions_ItemContent::class => [
+                'files' => [
+                    TaoItemsRoles::ITEM_CLASS_NAVIGATOR => ActionAccessControl::DENY,
+                ],
+                'delete' => [
+                    TaoItemsRoles::ITEM_CLASS_NAVIGATOR => ActionAccessControl::DENY,
+                ],
+                'upload' => [
+                    TaoItemsRoles::ITEM_CLASS_NAVIGATOR => ActionAccessControl::DENY,
+                ],
+            ],
+        ],
+    ];
+
+    public function getDescription(): string
     {
+        return 'Configure asset permissions for Item Content creator role.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        OntologyUpdater::syncModels();
+
+        $setRolesAccess = $this->propagate(new SetRolesAccess());
+        $setRolesAccess([
+            '--' . SetRolesAccess::OPTION_REVOKE,
+            '--' . SetRolesAccess::OPTION_CONFIG, self::REVOKE_CONFIG,
+        ]);
         $setRolesAccess = $this->propagate(new SetRolesAccess());
         $setRolesAccess([
             '--' . SetRolesAccess::OPTION_CONFIG, self::CONFIG,
+        ]);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $setRolesAccess = $this->propagate(new SetRolesAccess());
+        $setRolesAccess([
+            '--' . SetRolesAccess::OPTION_REVOKE,
+            '--' . SetRolesAccess::OPTION_CONFIG, self::CONFIG,
+        ]);
+        $setRolesAccess = $this->propagate(new SetRolesAccess());
+        $setRolesAccess([
+            '--' . SetRolesAccess::OPTION_CONFIG, self::REVOKE_CONFIG,
         ]);
     }
 }
