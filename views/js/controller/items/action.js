724d34ff0c4dde4d7e26d3d1e0caaa0070632869
Cristian Lagos <17564918+clagosarias@users.noreply.github.com>
2021-11-19 13:11:40 +0100

    fix: add check for triggering tree refresh and new message

--- views/js/controller/items/action.js
+++ views/js/controller/items/action.js
@@ -179,6 +179,11 @@ define([
                     uri: actionContext.uri || actionContext.classUri
                 });
             } else {
+                if (response.success && !response.deleted) {
+                    $(actionContext.tree).trigger('refresh.taotree');
+                    reject(response.msg || __('Unable to delete the selected resource because you do not have the required rights to delete part of its content.'));
+                }
+
                 reject(response.msg || __('Unable to delete the selected resource'));
             }
         });
